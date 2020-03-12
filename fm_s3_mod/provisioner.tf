c8e414c5129ee5471e70e37a474c45433bdcf499
fmbah <felix.mbah@matchesfashion.com>
2020-03-12 08:56:48 +0000

    0.0.2 changes on providers

---
+++ fm_s3_mod/provisioner.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+#   profile = "${var.profile_name}"
+  region  = "eu-west-1"
+  version = "~> 2.7"
+}
+
+terraform {
+  required_version = ">= 0.12.12"
+
+  backend "s3" {
+    bucket  = "dm-vpc-states"
+    key     = "modules_tests/terraform.tfstate"
+    region  = "eu-west-1"
+    encrypt = "true"
+  }
+}
