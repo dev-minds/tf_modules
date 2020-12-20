dd565f499086c993e4e1d8f5d34d8f9b41f15ff1
Felix Mbah <felixm@ukhoitmb3019.home>
2020-12-20 12:35:03 +0000

    Back to work on this one

--- fm_vpc_mod/dm_simple_vpc/output.tf
+++ fm_vpc_mod/dm_simple_vpc/output.tf
@@ -5,5 +5,5 @@ output "vpc_id_otp" {
 
 output "private_subnets" {
   description = "List of IDs of private subnets"
-  value       = aws_subnet.private.*.id
+  value       = aws_subnet.dm_priv_res.*.id
 }
